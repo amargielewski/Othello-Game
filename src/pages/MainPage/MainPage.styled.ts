83debe6fe4033840bf500be7ef5a1345242ad295
amargielewski <aleksander.margielewski4@gmail.com>
2022-02-20 01:09:26 +0100

    feat

--- src/pages/MainPage/MainPage.styled.ts
+++ src/pages/MainPage/MainPage.styled.ts
@@ -5,8 +5,23 @@ export const StyledWrapper = styled.div`
   align-items: center;
   flex-direction: column;
   gap: 20px 0;
+  margin-bottom: 50px;
 `;
 
 export const StyledPageTitle = styled.h2`
-  font-size: 24px;
+  font-size: 30px;
+  font-weight: 500;
+`;
+
+export const StyledGameBoard = styled.div`
+  background-color: #21a179;
+  border: 10px solid #fafafa;
+  display: grid;
+  grid-template-rows: repeat(8, 75px);
+  grid-template-columns: repeat(8, 75px);
+`;
+
+export const StyledSingleCell = styled.div`
+  width: 100%;
+  background-color: red;
 `;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,18 @@
-import React from "react";
-import { StyledPageTitle, StyledWrapper } from "./MainPage.styled";
+//styles
+import {
+  StyledGameBoard,
+  StyledPageTitle,
+  StyledSingleCell,
+  StyledWrapper,
+} from "./MainPage.styled";
 
 export const MainPage = () => {
   return (
     <StyledWrapper>
       <StyledPageTitle>Play Game</StyledPageTitle>
+      <StyledGameBoard>
+        <StyledSingleCell></StyledSingleCell>
+      </StyledGameBoard>
     </StyledWrapper>
   );
 };
